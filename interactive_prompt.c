f72075da4c3107ffff0cc13cba59502be56ea5c2
merryt <tymerry@gmail.com>
2023-02-06 08:19:13 +0100

    adding a parser

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
-int main(int argc, char** argv){
-    puts("=== Toylisp repl ====");
-    puts("Press Ctrl+c to exit /n");
-    for(;;){
-        char* input = readline("tlisp> ");
+int main(int argc, char** argv) {
+  mpc_parser_t* Number      = mpc_new("number");
+  mpc_parser_t* Operator    = mpc_new("operator");
+  mpc_parser_t* Expr        = mpc_new("expr");
+  mpc_parser_t* TLISP       = mpc_new("tlisp");
 
-        add_history(input);
+  mpca_lang(MPCA_LANG_DEFAULT, 
+    "                                                         \
+    number    : /-?[0-9]+/;                                   \
+    operator  : '+' | '-' | '*' | '/' | '%';                  \
+    expr      : <number> | '(' <operator> <expr>+ ')';        \
+    tlisp     : /^/ <operator> <expr>+ /$/ ;                  \
+    ",
+    Number, Operator, Expr, TLISP);
 
-        printf("NO your a %s", input);
-        free(input);
+
+  puts("Toy testing lisp repl");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+    char* input = readline("tlisp > ");
+    add_history(input);
+
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, TLISP, &r)){
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
     }
-    return 0;
+     
+
+    free(input);
+
+  }
+  mpc_cleanup(4, Number, Operator, Expr, TLISP);
+  return 0;
 }
