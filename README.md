65cc21be3b1244aede123f7f75512114ce27ace5
merryt <tymerry@gmail.com>
2023-02-06 20:29:43 +0100

    WIP: starting on eval

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # toylisp
+
 toy lisp language
+
+to compile maybe run
+`cc --std=c99 -Wall interactive_prompt.c mpc.c -ledit -lm -o interactive_prompt`
--- interactive_prompt.c
+++ interactive_prompt.c
@@ -9,17 +9,18 @@
 static char buffer[2048];
 
 /* Fake readline function */
-char* readline(char* prompt) {
+char *readline(char *prompt)
+{
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
+  char *cpy = malloc(strlen(buffer) + 1);
   strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
+  cpy[strlen(cpy) - 1] = '\0';
   return cpy;
 }
 
 /* Fake add_history function */
-void add_history(char* unused) {}
+void add_history(char *unused) {}
 
 /* Otherwise include the editline headers */
 #else
@@ -27,41 +28,49 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
-int main(int argc, char** argv) {
-  mpc_parser_t* Number      = mpc_new("number");
-  mpc_parser_t* Operator    = mpc_new("operator");
-  mpc_parser_t* Expr        = mpc_new("expr");
-  mpc_parser_t* TLISP       = mpc_new("tlisp");
+long eval(mpc_ast_t *t)
+{
+  return 0;
+}
 
-  mpca_lang(MPCA_LANG_DEFAULT, 
-    "                                                         \
-    number    : /-?[0-9]+/;                                   \
+int main(int argc, char **argv)
+{
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *TLISP = mpc_new("tlisp");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                         \
+    number    : /-?[0-9IO]+/;                                   \
     operator  : '+' | '-' | '*' | '/' | '%';                  \
     expr      : <number> | '(' <operator> <expr>+ ')';        \
     tlisp     : /^/ <operator> <expr>+ /$/ ;                  \
     ",
-    Number, Operator, Expr, TLISP);
-
+            Number, Operator, Expr, TLISP);
 
   puts("Toy testing lisp repl");
   puts("Press Ctrl+c to Exit\n");
 
-  while (1) {
-    char* input = readline("tlisp > ");
+  while (1)
+  {
+    char *input = readline("tlisp > ");
     add_history(input);
 
     mpc_result_t r;
-    if(mpc_parse("<stdin>", input, TLISP, &r)){
-      mpc_ast_print(r.output);
+    if (mpc_parse("<stdin>", input, TLISP, &r))
+    {
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
-    } else {
+    }
+    else
+    {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-     
 
     free(input);
-
   }
   mpc_cleanup(4, Number, Operator, Expr, TLISP);
   return 0;
