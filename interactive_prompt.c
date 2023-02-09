bb52f776acba24d1448fb66d2833b77b9c04e52f
merryt <tymerry@gmail.com>
2023-02-09 04:49:04 +0100

    error handling

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,7 +1,7 @@
+#include "mpc.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "mpc.h"
 
 /* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
@@ -10,140 +10,202 @@
 static char buffer[2048];
 
 /* Fake readline function */
-char *readline(char *prompt)
+char* readline(char* prompt)
 {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char *cpy = malloc(strlen(buffer) + 1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy) - 1] = '\0';
-  return cpy;
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
 }
 
 /* Fake add_history function */
-void add_history(char *unused) {}
+void add_history(char* unused) { }
 
 /* Otherwise include the editline headers */
 #else
-#include <editline/readline.h>
 #include <editline/history.h>
+#include <editline/readline.h>
 #endif
 
-long eval_op(long x, char *op, long y)
+typedef struct
 {
-  if (strcmp(op, "+") == 0)
-  {
-    return x + y;
-  }
-  if (strcmp(op, "-") == 0)
-  {
-    return x - y;
-  }
-  if (strcmp(op, "*") == 0)
-  {
-    return x * y;
-  }
-  if (strcmp(op, "/") == 0)
-  {
-    return x / y;
-  }
-  if (strcmp(op, "%") == 0)
-  {
-    return x % y;
-  }
-  if (strcmp(op, "^") == 0)
-  {
-    return pow(x, y);
-  }
-  if (strcmp(op, "min") == 0)
-  {
-    if (x < y)
-    {
-      return x;
-    }
-    else
-    {
-      return y;
-    }
-  }
-  if (strcmp(op, "max") == 0)
-  {
-    if (x > y)
-    {
-      return x;
-    }
-    else
-    {
-      return y;
-    }
-  }
-  return 0;
+    int type;
+    long num;
+    int err;
+} lval; // lval stands for Lisp VALue
+
+/* Create Enumeration of Possible lval Types */
+enum {
+    LVAL_NUM,
+    LVAL_ERR
+};
+
+/* Create Enumeration of Possible Error Types */
+enum {
+    LERR_DIV_ZERO,
+    LERR_BAD_OP,
+    LERR_BAD_NUM
+};
+
+/* Create a new number type lval */
+lval lval_num(long x)
+{
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t *t)
+/* Create a new error type lval */
+lval lval_err(int x)
 {
-  // If tagged as a number return it directly
-  if (strstr(t->tag, "number")) // strstr compares two strings, it finds the second paramater in the first
-  {
-    return atoi(t->contents); // atoi converts a string to an int
-  }
-
-  // the operator is always the second child
-  char *op = t->children[1]->contents;
-
-  // we store the third child in 'x'
-  long x = eval(t->children[2]);
-
-  // we store the third child in 'x'
-  int i = 3;
-  while (strstr(t->children[i]->tag, "expr"))
-  {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
-
-  return x;
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
 }
 
-int main(int argc, char **argv)
+// print an 'lval'
+void lval_print(lval v)
 {
-  mpc_parser_t *Number = mpc_new("number");
-  mpc_parser_t *Operator = mpc_new("operator");
-  mpc_parser_t *Expr = mpc_new("expr");
-  mpc_parser_t *TLISP = mpc_new("tlisp");
+    switch (v.type) {
+        // In the case the type is a number print it
+        // In the case the type is an error
+    case LVAL_NUM:
+        printf("%li", v.num);
+        break;
 
-  mpca_lang(MPCA_LANG_DEFAULT,
-            "                                                             \
+        // In the case the type is an error
+    case LVAL_ERR:
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division by zero!");
+        }
+        if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid Operator!");
+        }
+        if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid Number!");
+        }
+
+        break;
+    }
+}
+
+// Print an 'lval' followed by a newline
+void lval_println(lval v)
+{
+    lval_print(v);
+    putchar('\n');
+}
+
+lval eval_op(lval x, char* op, lval y)
+{
+    if (x.type == LVAL_ERR) {
+        return x;
+    }
+
+    if (x.type == LVAL_ERR) {
+        return y;
+    }
+
+    if (strcmp(op, "+") == 0) {
+        return lval_num(x.num + y.num);
+    }
+    if (strcmp(op, "-") == 0) {
+        return lval_num(x.num - y.num);
+    }
+    if (strcmp(op, "*") == 0) {
+        return lval_num(x.num * y.num);
+    }
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    }
+    if (strcmp(op, "%") == 0) {
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+    }
+    if (strcmp(op, "^") == 0) {
+        return lval_num(pow(x.num, y.num));
+    }
+    if (strcmp(op, "min") == 0) {
+        if (x.num < y.num) {
+            return lval_num(x.num);
+        } else {
+            return lval_num(y.num);
+        }
+    }
+    if (strcmp(op, "max") == 0) {
+        if (x.num > y.num) {
+            return lval_num(x.num);
+        } else {
+            return lval_num(y.num);
+        }
+    }
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t)
+{
+    // If tagged as a number return it directly
+    if (strstr(t->tag, "number")) // strstr compares two strings, it finds the second paramater in the first
+    {
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_num(LERR_BAD_NUM);
+    }
+
+    // the operator is always the second child
+    char* op = t->children[1]->contents;
+
+    // we store the third child in 'x'
+    lval x = eval(t->children[2]);
+
+    // we store the third child in 'x'
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+int main(int argc, char** argv)
+{
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* TLISP = mpc_new("tlisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                             \
     number    : /-?[0-9IO]+/;                                             \
     operator  : '+' | '-' | '*' | '/' | '%' | '^' | /(min)/ | /(max)/;    \
     expr      : <number> | '(' <operator> <expr>+ ')';                    \
     tlisp     : /^/ <operator> <expr>+ /$/ ;                              \
     ",
-            Number, Operator, Expr, TLISP);
+        Number, Operator, Expr, TLISP);
 
-  puts("Toy testing lisp repl");
-  puts("Press Ctrl+c to Exit\n");
+    puts("Toy testing lisp repl");
+    puts("Press Ctrl+c to Exit\n");
 
-  while (1)
-  {
-    char *input = readline("tlisp > ");
-    add_history(input);
+    while (1) {
+        char* input = readline("tlisp > ");
+        add_history(input);
 
-    mpc_result_t r;
-    if (mpc_parse("<stdin>", input, TLISP, &r))
-    {
-      long result = eval(r.output);
-      printf("%li\n", result);
-      mpc_ast_delete(r.output);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, TLISP, &r)) {
+            lval result = eval(r.output);
+            lval_println(result);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
     }
-    else
-    {
-      mpc_err_print(r.error);
-      mpc_err_delete(r.error);
-    }
-
-    free(input);
-  }
-  mpc_cleanup(4, Number, Operator, Expr, TLISP);
-  return 0;
+    mpc_cleanup(4, Number, Operator, Expr, TLISP);
+    return 0;
 }
